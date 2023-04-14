04eee1e69999fda786701c358660212f729f676b
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-04-15 01:27:50 +0200

    Update LinkButton.tsx

--- components/Reusable/LinkButton/LinkButton.tsx
+++ components/Reusable/LinkButton/LinkButton.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import style from './LinkButton.module.scss'
 
 
+
 interface LinkButtonProps {
 title: string;
 link: string;
 }
 
 const LinkButton:React.FC<LinkButtonProps> = ({title, link}) => {
+  
   return (
     <div className={style.wrapper}>
         <Link href={link}>
