7b859a60782e8a7de733b6f92d81430b4516ce7a
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-04-18 18:22:22 +0200

    work gif

--- components/Work/Work.tsx
+++ components/Work/Work.tsx
@@ -3,6 +3,7 @@ import style from "./Work.module.scss";
 import WorkSection from "../Home/WorkSection/WorkSection";
 import Clients from "../Reusable/Clients/Clients";
 import { WORK_LIST } from "../Reusable/WorkSingle/constant";
+import WorkGif from "./WorkGif/WorkGif";
 
 interface WorkProps {
   some?: string;
@@ -13,8 +14,13 @@ const Work: React.FC<WorkProps> = ({ some }) => {
     <div className={style.wrapper}>
       <h1 data-centered="yes">Work</h1>
       <WorkSection list={WORK_LIST} />
-      <Clients backgroundtype="black" centered='no' contentCentered="yes" title />
-      <p></p>
+      <WorkGif />
+      <Clients
+        backgroundtype="black"
+        centered="no"
+        contentCentered="yes"
+        title
+      />
     </div>
   );
 };
---
+++ components/Work/WorkGif/WorkGif.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+import style from "./WorkGif.module.scss";
+
+const WorkGif = () => {
+  return (
+    <section className={style.wrapper}>
+      <div className={style.image}>
+        <Image src="/assets/images/workGif.gif" fill alt="workgif" />
+      </div>
+    </section>
+  );
+};
+
+export default WorkGif;
