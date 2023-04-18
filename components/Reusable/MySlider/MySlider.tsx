d91f4e0342d64d6e6857238d0bd3211a501389ea
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-04-18 23:15:09 +0200

    arrows on slider testemonial

--- components/Reusable/MySlider/MySlider.tsx
+++ components/Reusable/MySlider/MySlider.tsx
@@ -5,46 +5,48 @@ import Image from "next/image";
 import style from "./MySlider.module.scss";
 
 interface SliderProps {
-    slidesPerView: number;
-    slidesListImages?: { alt: string; img: string }[];
-    slidesListTestemonial?: {
-      testemonial:string, 
-      author: string, 
-      jobDescription:string, 
-      company:string
-    }[];
-    sliderType?: 'images' | 'testemonial';
+  slidesPerView: number;
+  slidesListImages?: { alt: string; img: string }[];
+  slidesListTestemonial?: {
+    testemonial: string;
+    author: string;
+    jobDescription: string;
+    company: string;
+  }[];
+  sliderType?: "images" | "testemonial";
 }
 
-const MySlider: React.FC<SliderProps> = ({  slidesPerView=2, slidesListImages=[],slidesListTestemonial=[],sliderType='images'}) => {
+const MySlider: React.FC<SliderProps> = ({
+  slidesPerView = 2,
+  slidesListImages = [],
+  slidesListTestemonial = [],
+  sliderType = "images",
+}) => {
 
   const settings = {
     dots: false,
-     infinite: true,
-    arrows: sliderType === 'testemonial',
+    infinite: true,
+    arrows: sliderType === "testemonial",
     speed: 500,
     slidesToShow: slidesPerView,
     slidesToScroll: 1,
     adaptiveHeight: true,
     swipeToSlide: true,
-    nextArrow: <p>e</p>,
-    prevArrow: <p>d</p>
+
   };
 
-  const wrapperStyle =cn(
-    style.wrapper,
-    {'testemonialSlider': sliderType === 'testemonial'},
-  )
-  const slideSingleStyle =cn(
-    {[style.slideSingleImage]: sliderType === 'images'},
-    {[style.slideSingleTestemonial]: sliderType === 'testemonial'},
-  )
+  const wrapperStyle = cn(style.wrapper, {
+    testemonialSlider: sliderType === "testemonial",
+  });
+  const slideSingleStyle = cn(
+    { [style.slideSingleImage]: sliderType === "images" },
+    { [style.slideSingleTestemonial]: sliderType === "testemonial" }
+  );
 
   const generateSlidesImages = () => {
     return slidesListImages.map((item) => {
       return (
         <div key={item.alt} className={style.singleSlideWrapper}>
-
           <div className={slideSingleStyle}>
             <Image src={item.img} alt={item.alt} fill />
           </div>
@@ -56,19 +58,23 @@ const MySlider: React.FC<SliderProps> = ({  slidesPerView=2, slidesListImages=[]
   const generateSlidesTestemonial = () => {
     return slidesListTestemonial.map((item) => {
       return (
-          <div key={item.author}className={slideSingleStyle}>
-            <p>{item.testemonial}</p>
-            <p>{item.author}</p>
-            <p>{item.jobDescription}</p>
-            <p>{item.company}</p>
-          </div>
+        <div key={item.author} className={slideSingleStyle}>
+          <p>{item.testemonial}</p>
+          <p>{item.author}</p>
+          <p>{item.jobDescription}</p>
+          <p>{item.company}</p>
+        </div>
       );
     });
   };
 
   return (
     <section className={wrapperStyle}>
-      <Slider {...settings}>{slidesListImages.length > 0 ? generateSlidesImages() : generateSlidesTestemonial()}</Slider>
+      <Slider  {...settings}>
+        {slidesListImages.length > 0
+          ? generateSlidesImages()
+          : generateSlidesTestemonial()}
+      </Slider>
     </section>
   );
 };
