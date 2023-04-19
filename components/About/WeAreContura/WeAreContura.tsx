9e6992ba5cfe3652964dee6fda7fb800f621b50a
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-04-19 13:59:20 +0200

    about page ractor

--- components/About/WeAreContura/WeAreContura.tsx
+++ components/About/WeAreContura/WeAreContura.tsx
@@ -9,11 +9,11 @@ const WeAreContura: React.FC<WeAreConturaProps> = ({ some }) => {
   return (
     <div className={style.wrapper}>
       <div className={style.centralPart}>
-          <h1>Hey, we're ConturaDesign</h1>
-          <h4>
-            We create world-class digital products, web design, and branding.
-            professionals who will help your business reach the top.
-          </h4>
+        <h1>Hey, we're ConturaDesign</h1>
+        <h4>
+          We create world-class digital products, web design, and branding.
+          professionals who will help your business reach the top.
+        </h4>
       </div>
     </div>
   );
