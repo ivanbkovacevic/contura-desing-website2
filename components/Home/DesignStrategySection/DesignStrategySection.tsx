6a6425a7e42573aaf769daaf26e2093fdc8c2b90
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-04-20 22:34:05 +0200

    Accordions new

--- components/Home/DesignStrategySection/DesignStrategySection.tsx
+++ components/Home/DesignStrategySection/DesignStrategySection.tsx
@@ -5,29 +5,25 @@ import { DESIGN_STRATEGY_LIST } from "./constants";
 import style from "./DesignStrategySection.module.scss";
 
 const DesignStrategySection: React.FC = ({}) => {
-  const [idAccordion, setIdAccordion] = useState<number | null>(null);
+  const [idAccordion, setIdAccordion] = useState<number[]>([]);
 
   const toggleAcc = (id: number) => {
-    if (idAccordion === id) {
-      setIdAccordion(null);
+    if (idAccordion.includes(id)) {
+      const newAcc = idAccordion.filter((item) => item !== id);
+      setIdAccordion(newAcc);
       return;
     }
-    setIdAccordion(id);
+    setIdAccordion([...idAccordion, id]);
   };
 
   const accordions = () => {
     return DESIGN_STRATEGY_LIST.map((item, idx) => {
-      let showAccordion = false;
-      if (idAccordion === idx) {
-        showAccordion = true;
-      }
-
       return (
         <Accordion
           key={item.title}
           title={item.title}
           content={item.content}
-          show={showAccordion}
+          show={idAccordion.includes(idx)}
           accClicked={() => toggleAcc(idx)}
         />
       );
