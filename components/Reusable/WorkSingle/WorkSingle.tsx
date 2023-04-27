249d856ba536965e056f91a9ef29165d0fed452a
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-04-27 23:42:36 +0200

    learn more move

--- components/Reusable/WorkSingle/WorkSingle.tsx
+++ components/Reusable/WorkSingle/WorkSingle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AOS from "aos";
 import Image from "next/image";
 import cn from "classnames";
@@ -16,21 +16,56 @@ const WorkSingle: React.FC<Work> = ({
   link,
   descFontColor,
 }) => {
+  const elementRef = useRef(null);
+  const [rectLeft, setRectLeft] = useState(null);
+  const [rectTop, setRectTop] = useState(null);
+  const [cursorPosition, setCursorPosition] = useState({ x: null, y: null });
+  const [spanPosition, setSpanPosition] = useState({ x: null, y: null });
+
+  useEffect(() => {
+    const element = elementRef.current;
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      setRectLeft(rect.left);
+      setRectTop(rect.top);
+
+      setSpanPosition({
+        y: cursorPosition.y - (rectTop + 50),
+        x: cursorPosition.x - rectLeft,
+      });
+    }
+  }, [cursorPosition]);
+
   const wrapperImgAndDescription = cn(
     style.wrapperImgAndDescription,
     { [style.orderImageUp]: order === ORDER.PICTURE_UP },
     { [style.orderImageDown]: order === ORDER.PICTURE_DOWN }
   );
 
+  function handleMouseMove(event: { clientX: any; clientY: any }) {
+    const x = event.clientX;
+    const y = event.clientY;
+    setCursorPosition({ x, y });
+  }
+
   return (
     <a href={link} className={style.wrapper} target="_blank">
       <div
+        ref={elementRef}
+        onMouseMove={handleMouseMove}
         data-aos="fade-up"
         data-aos-duration="1000"
         key={title}
         className={wrapperImgAndDescription}
       >
-        <span>Learn more</span>
+        <span
+          style={{
+            top: spanPosition.y,
+            left: spanPosition.x,
+          }}
+        >
+          Learn more
+        </span>
         <div className={style.description} style={{ backgroundColor: color }}>
           <h3 style={{ color: descFontColor }}>{description}</h3>
         </div>
