964f9099e3bd586787600a925d287b05c2d856e4
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-05-12 14:15:11 +0200

    worksection

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import HeroSection from "./HeroSection/HeroSection";
 import DesignStrategySection from "./DesignStrategySection/DesignStrategySection";
-import Clients from "../Reusable/Companies/Companies";
+import Companies from "../Reusable/Companies/Companies";
 import ButtonLink from "../Reusable/ButtonLink/ButtonLink";
 import WorkSection from "./WorkSection/WorkSection";
 import CompanyNumbers from "./CompanyNumbers/CompanyNumbers";
-import ProductDevelopment from "../ProductDevelopment/UIUXDesign/UIUXDesign";
 import FAQ from "./FAQ/FAQ";
 import MySlider from "../Reusable/MySlider/MySlider";
 import { SLIDES_HOME } from "../Reusable/MySlider/constant";
@@ -25,9 +24,9 @@ const Home = () => {
       <section className={style.designStrategySection} data-centered="yes">
         <DesignStrategySection />
       </section>
-      <section className={style.clientsSection} data-centered="yes">
+      <section className={style.companiesSection} data-centered="yes">
         <h3 className={style.title}>People & brands love it</h3>
-        <Clients list={BIG_COMPANIES} companieType="bigCompanies" />
+        <Companies list={BIG_COMPANIES} companieType="bigCompanies" />
       </section>
       <section className={style.buttonLinkSection} data-centered="yes">
         <ButtonLink title="see more work" link="/work" />
@@ -47,9 +46,6 @@ const Home = () => {
       <section className={style.sliderSection}>
         <MySlider slidesPerView={3} slidesListImages={SLIDES_HOME} />
       </section>
-      {/* <section className={style.productDevelopment} data-centered="yes">
-        <ProductDevelopment />
-      </section> */}
       <section className={style.faqSection}>
         <FAQ />
       </section>
