ce70a2cf1ce02d318217bf2436f04b8a8bc99ce9
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-05-14 18:22:49 +0200

    what we do on About

--- components/About/WhatWeDo/WhatWeDo.tsx
+++ components/About/WhatWeDo/WhatWeDo.tsx
@@ -8,18 +8,18 @@ const WhatWeDo: React.FC = ({}) => {
       <h1>What We Do</h1>
       <div className={style.singleWhatWeDo}>
         <div className={style.description}>
-          <div>
+          <div className={style.descriptionText}>
             <h2>Digital products that people love to use</h2>
             <p>
               By applying behavioral science to customer experience, we design
               engaging digital products used by millions of people.
             </p>
           </div>
-          <div className={`${style.descriptionImage} ${style.negativeLeft}`}>
+          <div className={`${style.descriptionImage} ${style.negativeLeft} ${style.imageDesc1}`}>
             <Image src="/assets/images/about/aboutImagePrva.png" alt="image" fill />
           </div>
         </div>
-        <div className={style.image}>
+        <div className={`${style.image} ${style.image1}`}>
           <Image quality={100}
             src="/assets/images/about/aboutMedtisimageDruga.png"
             alt=""
@@ -27,16 +27,23 @@ const WhatWeDo: React.FC = ({}) => {
           />
         </div>
       </div>
-      <div className={`${[style.singleWhatWeDo]} ${[style.reverse]}`}>
+      <div className={style.singleWhatWeDo}>
+      <div className={`${style.image} ${style.image2}`}>
+          <Image quality={100}
+            src="/assets/images/about/aboutForwardizeImageTreca.png"
+            alt=""
+            fill
+          />
+        </div>
         <div className={style.description}>
-          <div>
+          <div className={style.descriptionText}>
             <h2>Enterprise software that doesn't suck</h2>
             <p>
               We create human-centered enterprise software that has the
               polished, snappy feel of the best consumer apps.
             </p>
           </div>
-          <div className={`${style.descriptionImage} ${style.negativeRight}`}>
+          <div className={`${style.descriptionImage} ${style.negativeRight} ${style.imageDesc2}`}>
             <Image quality={100}
               src="/assets/images/about/aboutStatisikaImageCetvrta.png"
               alt=""
@@ -44,24 +51,21 @@ const WhatWeDo: React.FC = ({}) => {
             />
           </div>
         </div>
-        <div className={style.image}>
-          <Image quality={100}
-            src="/assets/images/about/aboutForwardizeImageTreca.png"
-            alt=""
-            fill
-          />
-        </div>
       </div>
+
+
+
+
       <div className={style.singleWhatWeDo}>
         <div className={style.description}>
-          <div>
+          <div className={style.descriptionText}>
             <h2>Websites that tell your brand's story</h2>
             <p>
               From Slack to Facebook, we team up with marketing departments of
               large companies and startups to deliver award‑winning websites.
             </p>
           </div>
-          <div className={style.descriptionImage}>
+          <div className={`${style.descriptionImage} ${style.imageDesc3}`}>
             <Image quality={100}
               src="/assets/images/about/aboutMobileKontturaImageSesta.png"
               alt=""
@@ -69,7 +73,7 @@ const WhatWeDo: React.FC = ({}) => {
             />
           </div>
         </div>
-        <div className={style.image}>
+        <div className={`${style.image} ${style.image3} `}>
           <Image quality={100}
             src="/assets/images/about/aboutFacebookLaptopImagePeta.png"
             alt=""
