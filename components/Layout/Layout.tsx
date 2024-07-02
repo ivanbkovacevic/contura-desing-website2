dd2e96cd217f28ff8afe1c856b776e70acd1aa48
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2024-07-02 23:16:27 +0200

    this is a demo

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,8 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect } from "react";
 import AOS from "aos";
 import Head from "next/head";
 import Header from "./Header/Header";
 import style from "./Layout.module.scss";
-import Footer from "./Footer/Footer";
 import FooterGrid from "./FooterGrid/FooterGrid";
 
 type Props = {
@@ -12,33 +11,22 @@ type Props = {
 };
 
 const Layout = ({ children, title = "Contura Design" }: Props) => {
-  const [black, setBlack] = useState('');
   useEffect(() => {
     AOS.init({});
-    const userInput = window.prompt('Enter the code that I have give you:');
-   
-    setBlack(userInput)
   }, []);
-   
+
   return (
     <div className={style.wrapper}>
-      {black === "MishaIViktorijaSkacuPoBarama" && (
-        <>
-          <Head>
-            <title>{title}</title>
-            <meta charSet="utf-8" />
-            <meta
-              name="viewport"
-              content="initial-scale=1.0, width=device-width"
-            />
-            <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
-          </Head>
-          <Header />
-          <main>{children}</main>
-          {/* <Footer /> */}
-          <FooterGrid />
-        </>
-      )}
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+      </Head>
+      <Header />
+      <div className={style.demo}>THIS IS A DEMO</div>
+      <main>{children}</main>
+      <FooterGrid />
     </div>
   );
 };
